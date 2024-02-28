fe7be330897a455ffa96eb93a30997e20c567269
picalexs@gmail.com <picalexs@gmail.com>
2024-02-28 14:24:27 +0200

    Separare cuvinte

--- Lab1/sortWords/sortWords.cpp
+++ Lab1/sortWords/sortWords.cpp
@@ -1,30 +1,39 @@
-#include <iostream>
 #include <string>
 #include <stdio.h>
+#include <algorithm>
 using namespace std;
+
+bool compareWords(string w1, string w2) {
+    if (w1.length() == w2.length()) {
+        return w1 < w2;
+    }
+    return w1.length() > w2.length();
+}
 
 int main() {
     char sentence[256];
-    scanf("%[^\n]", sentence);
+    fgets(sentence, 256, stdin);
     string str = sentence;
     string words[100];
 
     bool isWord = true;
-    int pozStartWord = 0;
-    int wordIndex = 0;
+    int startWordIdx = 0;
+    int wordIdx = 0;
 
     for (int i = 0; i < str.size(); i++) {
-        if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0')) {
-            words[wordIndex++] = str.substr(pozStartWord, i - pozStartWord + 1);
+        if (str[i] != ' ' && (i + 1 == str.size() || str[i + 1] == ' ')) {
+            words[wordIdx++] = str.substr(startWordIdx, i - startWordIdx + 1);
         }
         if (str[i] == ' ') {
-            pozStartWord = i + 1;
+            startWordIdx = i + 1;
         }
     }
-    
-    for (int i = 0; i < wordIndex; i++) {
-        cout << words[i] << endl;
-    }
 
+    words[wordIdx-1].pop_back();//sterger \n de la ultimul cuvant
+    sort(words, words + wordIdx, compareWords);
+
+    for (int i = 0; i < wordIdx; i++) {
+        printf("%s\n", words[i].c_str());
+    }
     return 0;
 }
