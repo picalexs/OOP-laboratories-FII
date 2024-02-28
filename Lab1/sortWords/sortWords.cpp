08a65963a4be608c80ac1216771b1fdf026db534
picalexs@gmail.com <picalexs@gmail.com>
2024-02-28 21:07:41 +0200

    Am uitat sa dau commit la versiunea noua

--- Lab1/sortWords/sortWords.cpp
+++ Lab1/sortWords/sortWords.cpp
@@ -2,18 +2,18 @@
 #include <stdio.h>
 using namespace std;
 
-bool compareWords(string w1, string w2) {
+bool hasToSwap(string w1, string w2) {
     if (w1.length() == w2.length()) {
-        return w1 < w2;
+        return w1 >= w2;
     }
-    return w1.length() > w2.length();
+    return w1.length() <= w2.length();
 }
 
 void sortWords(string words[], int wordsIdx){
     for(int i=0;i<wordsIdx-1;i++){
         for(int j=i+1;j<wordsIdx;j++)
         {
-            if(!compareWords(words[i],words[j])){
+            if(hasToSwap(words[i],words[j])){
                 swap(words[i],words[j]);
             }
         }
@@ -41,7 +41,7 @@ int main() {
 
     words[wordIdx-1].pop_back();//sterger \n de la ultimul cuvant
     sortWords(words,wordIdx);
-    
+
     for (int i = 0; i < wordIdx; i++) {
         printf("%s\n", words[i].c_str());
     }
